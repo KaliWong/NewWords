d844df18c7d3a98839fea7b48a9ee7054ee44405
admin <admin@dyn-118-139-67-153.its.monash.edu.au>
2016-03-30 15:03:22 +1100

    Added Stage 2

--- Client/index.js
+++ Client/index.js
@@ -1,5 +1,28 @@
 var myApp = angular.module('myApp',[])
 
-myApp.controller('MyController', function($scope) {
-  $scope.newWord = 'cat'
+myApp.service('HistoryService',function($http){
+	var baseUrl = "http://localhost:8080/"
+
+	this.saveWord = function (newWord) { //set up the saveWord function
+		var url = baseUrl + "saveCurrent" // add the address of the saveCurrent to the baseUrl
+	    return $http.post(url, {"word": newWord}) // output newWord to the server
+	}
+
+})
+
+myApp.controller('MyController', function($scope, HistoryService) { // now the controller can use the HistoryService as well 
+  	$scope.newWord = 'cat'
+
+  	$scope.saveThisWord = function (){ 
+  		HistoryService.saveWord($scope.newWord) // use the saveWord function in History Service
+  		.then(saveSuccess, error) //svaeSucess function or error function
+  	}
+
+  	function saveSuccess (json) {
+    	console.log(json) // print out at client side console i.e. on browser -> ctl+shift+i
+  	}
+
+  	function error (err) {
+    	console.log(err)
+  	}
 })
--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,30 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser') 
+//include the body-parser that we just downloaded through npm install body-parser --save 
+var Nedb = require('nedb') // npm install nedb --save 
+var database = new Nedb({ filename: './data/data.db', autoload: true })
+// a server that connect to the database, save the data in a file (?)
 
 app.use(express.static('../Client')) // app.use: sit there and wait for a request.
 // .. goes back one folder then /Client goes to the Client folder
+
+app.use(bodyParser.json())
+
+app.post('/saveCurrent', function (req, res) {
+    var data = 
+    { 
+    	word: req.body.word,  // word property from request.body.word
+    	date: Date.now() 	  // date property
+    }
+
+	var done = function () {
+    	console.log('I just wrote to the database') // this is a server.js so this mag is shown on the server side console; Terminal
+    	res.end("done")
+	}
+
+	database.insert(data, done) // insert data in database and then run  
+
+})
 
 app.listen(8080) // This server is listening to this part. computer port: 8080
