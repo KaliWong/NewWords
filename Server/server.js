92ea7aaca72e0dd55dc99acadefef7608e04d000
admin <admin@dyn-118-139-67-153.its.monash.edu.au>
2016-03-30 11:58:23 +1100

    commit to changes

---
+++ Server/server.js
@@ -0,0 +1,7 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static('../Client')) // app.use: sit there and wait for a request.
+// .. goes up a folder then /Client goes to the Client folder
+
+app.listen(8080) // This server is listening to this part. computer port: 8080
